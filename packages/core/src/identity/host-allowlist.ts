8083c6162fa1a2b218419bb311b22aa8a6038187
zhang zhixuan <zhangzhiquan312@gmail.com>
2026-05-17 20:37:41 +0800

    chore(identity): delete dormant files (cwd-validator, host-allowlist, ssrf-guard, barrel, dead tests)

--- packages/core/src/identity/host-allowlist.ts
+++
@@ -1,7 +0,0 @@
-export class HostAllowlist {
-  constructor(private allowed: Set<string>) {}
-
-  check(host: string): void {
-    if (!this.allowed.has(host)) throw new Error(`host not allowed: ${host}`);
-  }
-}
